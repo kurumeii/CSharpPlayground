c9321dec295d7ecd6c903ef94f55b9422c6ddeca
kurumeii <kurumeii@users.noreply.github.com>
2020-12-07 21:35:50 +0700

    add vonglap

---
+++ tutorial/tutorial/learning/VongLap/vonglapfor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tutorial1.learning.VongLap
+{
+    class Vonglapfor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop example");
+
+            for(int i =2; i <15; i += 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Value of x = {0}", i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
