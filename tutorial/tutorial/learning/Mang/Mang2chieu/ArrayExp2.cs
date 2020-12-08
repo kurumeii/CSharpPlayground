d37c6e11c1b4d428fd5bafca4d10e51cd989c860
kurumeii <babybangunny@gmail.com>
2020-12-08 10:33:08 +0700

    again

---
+++ tutorial/tutorial/learning/Mang/Mang2chieu/ArrayExp2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mang2chieu
+{
+    class ArrayExp2
+    {
+        static void Main(string[] args)
+        {
+            //Khai báo mảng 2 chiều, 3 row, 4 col và gán giá trị
+            int[,] a =
+            {
+                { 1, 2, 3, 4},
+                { 5, 6, 7, 8},
+                { 9, 10, 11, 12}
+            };
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    Console.WriteLine("Ele at [{0}, {1}] = {2}", row, col, a[row, col]);
+                }
+            }
+            //không gán giá trị
+            int[,] b = new int[3, 4];
+            Console.WriteLine();
+        }
+    }
+}
